e03de95c08d898df8a4a5685fb4350f7a6f1c799
Unknown <riderjensen@gmail.com>
2018-12-14 12:57:54 -0700

    Encoding is working

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const http = require('http');
 
+
+function createDiv(val) {
+	let div;
+	if (val == 0) {
+		div = '<div style="width: 10px; height: 10px; display: inline-block; background-color: #fff;"></div>';
+	} else {
+		div = '<div style="width: 10px; height: 10px; display: inline-block; background-color: #000;"></div>';
+	}
+	return div;
+}
+
 const ourServer = http.createServer((req, res) => {
 	let ourURLArray = req.url.split('?')
 	if (ourURLArray[0] == '/getDat') {
+
+
 		let theUserValueArray = ourURLArray[1].split('=')
 		let theValue = theUserValueArray[1];
 		theValue = theValue.toUpperCase();
@@ -10,11 +23,33 @@ const ourServer = http.createServer((req, res) => {
 		for (let i = 0; i < theValue.length; i++) {
 			numberArray.push((theValue.charCodeAt(i) - 65).toString(2));
 		}
+		let theEncodedArrayOfArrays = [];
+		numberArray.forEach((item, i, array) => {
+			while (array[i].length < 8) {
+				array[i] = `0${array[i]}`;
+			}
+			let theSplitArray = array[i].split('');
+			theEncodedArrayOfArrays.push(theSplitArray)
+		})
 
-		console.log(theValue);
-		console.log(numberArray);
-	}
-	if (req.url = '/') {
+
+		console.log(theEncodedArrayOfArrays);
+		let returnedHTML = '<div id="theContainer">';
+		theEncodedArrayOfArrays.forEach((item, index) => {
+			returnedHTML += '<div class="myRow">';
+			item.forEach((anotherItem) => {
+				returnedHTML += createDiv(anotherItem);
+			})
+			returnedHTML += '</div>';
+		})
+		returnedHTML += '</div>';
+
+		res.write('<!DOCTYPE html> <html lang = "en" > ')
+		res.write('<head></head><body>');
+		res.write(`${returnedHTML}`)
+		res.write('</body></html>')
+		res.end();
+	} else {
 		res.write('<!DOCTYPE html> <html lang = "en" > ')
 		res.write('<head></head><body>');
 		res.write('<form action="/getDat" method="GET"><input type="text" value="google.com" name="addUser" /><button type="submit">Submit</button></form>')
