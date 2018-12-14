0de9eda2c1ee3ee2686182babb1b4309b0f65592
Rider <riderjensen@gmail.com>
2018-12-14 16:18:56 -0700

    Adding image path to upload

--- app.js
+++ app.js
@@ -50,10 +50,16 @@ const ourServer = http.createServer((req, res) => {
 		res.write('<script>function myLoadedFunction(){html2canvas(document.querySelector("#theContainer")).then(canvas => {document.body.appendChild(canvas)})};</script>')
 		res.write('<script onload="myLoadedFunction()" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.js"></script></body></html>');
 		res.end();
-	} else {
+	} else if(ourURLArray[0] == '/readDat') {
+		console.log(req);
+		res.write('');
+		res.end();
+	}else {
 		res.write('<!DOCTYPE html> <html lang = "en" > ')
 		res.write('<head></head><body>');
 		res.write('<form action="/getDat" method="GET"><input type="text" value="google.com" name="addUser" /><button type="submit">Submit</button></form>')
+		res.write('<br />');
+		res.write('<form action="/readDat" method="GET"><input type="file" accept="image/*" name="addUser" /><button type="submit">Submit</button></form>')		
 		res.write('</body></html>')
 		res.end();
 	}
