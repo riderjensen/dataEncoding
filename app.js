29da5f1a9b2d435879f1427bb77f46d74bdead90
Rider <riderjensen@gmail.com>
2018-12-13 21:22:07 -0700

    Getting char codes

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const ourServer = http.createServer((req, res) => {
 	if(ourURLArray[0] == '/getDat'){
 		let theUserValueArray = ourURLArray[1].split('=')
 		let theValue = theUserValueArray[1];
+		theValue = theValue.toUpperCase();
+		let numberArray = [];
+		for(let i = 0; i < theValue.length; i++){
+			numberArray.push(theValue.charCodeAt(i) - 65)
+		}
+
 		console.log(theValue);
+		console.log(numberArray);
 	}
 	if(req.url = '/'){
 		res.write('<!DOCTYPE html> <html lang = "en" > ')
 		res.write('<head></head><body>');
-		res.write('<form action="/getDat" method="GET"><input type="text" value="testing" name="addUser" /><button type="submit">Submit</button></form>')
+		res.write('<form action="/getDat" method="GET"><input type="text" value="google.com" name="addUser" /><button type="submit">Submit</button></form>')
 		res.write('</body></html>')
 		res.end();
 	}
